03975358fa58e2c971738ab70eb4b6dd489fc78f
samuelESP <samuelnegocios1509@gmail.com>
2024-03-02 10:05:58 -0300

    segregação do JavaScript

---
+++ js/segregacao.js
@@ -0,0 +1,9 @@
+console.log("Olá mundo Devaria...");
+        console.log(`Data Hora: ${ new Date()}`)
+
+        function menssage(){
+            alert("Chamamos um modal")
+        }
+        function menssage2(){
+            alert("Chamamos um modal2")
+        }  
