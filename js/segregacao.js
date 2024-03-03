b668df536d676c9fe07aa4fb9810e55dfed53398
samuelESP <samuelnegocios1509@gmail.com>
2024-03-03 14:00:08 -0300

    Arow Fuction, funções, variaveis e escopo

--- js/segregacao.js
+++ js/segregacao.js
@@ -1,9 +1,18 @@
-console.log("Olá mundo Devaria...");
-        console.log(`Data Hora: ${ new Date()}`)
 
-        function menssage(){
-            alert("Chamamos um modal")
-        }
-        function menssage2(){
-            alert("Chamamos um modal2")
-        }  
+const dataAtual = new Date();
+const msgFinal = "Vlw, e Flw"
+console.log("Olá mundo Devaria...");
+console.log(`Data Atual é ${dataAtual}`);
+
+function menssage1(chamada){
+    alert(chamada)
+}
+function menssage2(){
+    menssage1("Bom Dia Devaria")
+}
+
+const mensagemFinal = (msg1) => console.log(msg1);
+
+mensagemFinal(msgFinal);
+
+
