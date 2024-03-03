2e01654193ae40d8cd3f53da5645b85ca2e6ce9a
samuelESP <samuelnegocios1509@gmail.com>
2024-03-03 15:18:10 -0300

    Operadores

---
+++ js/operadores.js
@@ -0,0 +1,19 @@
+const numero1 = 32;
+const numero2 = 24;
+const numero3 = 14;
+const numero4 = 1;
+const stringNumero = '1';
+
+console.log('é igual', numero4 == stringNumero);
+console.log('é igual', numero4 === stringNumero);
+console.log('é diferente', numero4 != stringNumero);
+console.log('é diferente', numero4 !== stringNumero);
+
+
+
+
+console.log('Adição:', numero2 + numero1);
+console.log('Subtração:', numero4 - numero1);
+console.log('Multiplicação:', numero3 * numero1);
+console.log('Divisão:', (numero1 / numero3).toFixed(2));
+console.log('Módulo:', numero2 % numero3);
--- js/segregacao.js
+++ js/segregacao.js
@@ -16,3 +16,36 @@ const mensagemFinal = (msg1) => console.log(msg1);
 mensagemFinal(msgFinal);
 
 
+const verdadeiro = true;
+const naoVerdadeiro = !verdadeiro;
+const tudoEVerdadeiro = verdadeiro && naoVerdadeiro;
+const tudoEFalso = !verdadeiro && naoVerdadeiro;
+const algumEverdadeiro = verdadeiro || naoVerdadeiro;
+const algumEFalso = verdadeiro || naoVerdadeiro;
+
+console.log('verdadeiro', verdadeiro);
+console.log('naoVerdadeiro', naoVerdadeiro);
+console.log('tudoEVerdadeiro', tudoEVerdadeiro);
+console.log('tudoEFalso', tudoEFalso);
+console.log('algumEverdadeiro', algumEverdadeiro);
+console.log('algumEFalso', algumEFalso);
+
+if (1=="1") {
+    // == --> Compara o valor
+    console.log("São equivalentes");
+}
+
+if (1 === 1) {
+    // === --> Compara o valor e o tipo
+    console.log("São idênticos");
+}
+
+if (1 != 2) {
+    // != --> Compara o valor
+    console.log("1 é diferente de 2");
+}
+  
+if (1 !== "1") {
+    // !== --> Compara o valor e o tipo
+    console.log("1 numérico é diferente de 1 como string");
+}
